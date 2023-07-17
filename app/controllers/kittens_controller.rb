8030f7014b51dafecc8c3c60d99f5316be4a471f
Travis Williams <35243732+traviswilliams997@users.noreply.github.com>
2023-07-17 05:21:09 -0500

    Create kittens controller and routes

---
+++ app/controllers/kittens_controller.rb
@@ -0,0 +1,22 @@
+class KittensController < ApplicationController
+  def show
+  end
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
