4c47108a6b5f8ad0cc977eba47f8b7b2b4d48020
Travis Williams <35243732+traviswilliams997@users.noreply.github.com>
2023-07-17 05:33:07 -0500

    Add controller actions

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,22 +1,53 @@
 class KittensController < ApplicationController
   def show
+    @kitten = Kitten.find(params[:id])
   end
 
   def index
+    @kittens = Kitten.all
   end
 
   def new
+    @kitten = Kitten.new 
   end
 
   def create
+    @kitten = Kitten.new(kitten_params)
+
+    if @kitten.save
+      redirect_to @kitten
+    else
+      render :new, status: :unprocessable_entity
+    end
+
   end
 
   def edit
+    @kitten = Kitten.find(params[:id])
+
   end
 
   def update
+    @kitten = Kitten.find(params[:id])
+
+    if @kitten.update
+      redirect_to @kitten
+    else
+      render :edit, status: :unprocessable_entity
+    end
+
+
   end
 
   def destroy
+    @kitten = Kitten.find(params[:id])
+    @kitten.destroy
+
+    redirect_to root_path, status: :see_other
   end
+
+  private 
+  def kitten_params
+    params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+  end 
 end
